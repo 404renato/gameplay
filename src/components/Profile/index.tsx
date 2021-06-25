ea54ae9a125bed5b3f4372d0520ea0a4262d8ff1
Renato Santos <404renato@gmail.com>
2021-06-25 19:40:26 -0300

    Modal Sign Out

--- src/components/Profile/index.tsx
+++ src/components/Profile/index.tsx
@@ -1,30 +1,23 @@
-import React from 'react'
-import { View, Text, Alert } from 'react-native'
+import React, { useState } from 'react'
+import { View, Text, Modal, Alert, TouchableOpacity } from 'react-native'
 import { RectButton } from 'react-native-gesture-handler'
 
 import { styles } from './styles'
 import { Avatar } from './../Avatar'
 import { useAuth } from '../../hooks/auth'
+import { theme } from '../../global/styles/theme'
 
 export function Profile() {
   const { user, signOut } = useAuth()
-
-  function handleSignOut() {
-    Alert.alert('Logout', 'Deseja sair do GamePlay?', [
-      {
-        text: 'Não',
-        style: 'cancel',
-      },
-      {
-        text: 'Sim',
-        onPress: () => signOut(),
-      },
-    ])
-  }
+  const [visible, setVisible] = useState(false)
 
   return (
     <View style={styles.container}>
-      <RectButton onPress={handleSignOut}>
+      <RectButton
+        onPress={() => {
+          setVisible(!visible)
+        }}
+      >
         <Avatar urlImage={user.avatar} />
       </RectButton>
       <View>
@@ -36,6 +29,68 @@ export function Profile() {
 
         <Text style={styles.message}> Hoje é dia de vitória</Text>
       </View>
+
+      <Modal
+        transparent
+        statusBarTranslucent
+        visible={visible}
+        animationType="slide"
+      >
+        <View style={styles.modalContainer}>
+          <View
+            style={{
+              flexDirection: 'row',
+              marginBottom: 90,
+            }}
+          >
+            <Text style={styles.decision}>Deseja sair do Game</Text>
+
+            <Text style={[styles.decision, { color: theme.colors.primary }]}>
+              Play
+            </Text>
+
+            <Text style={styles.decision}>?</Text>
+          </View>
+
+          <View style={styles.decisionContainer}>
+            <TouchableOpacity
+              activeOpacity={0.7}
+              onPress={() => setVisible(false)}
+              style={[
+                styles.decisionButton,
+                { backgroundColor: 'transparent' },
+              ]}
+            >
+              <Text
+                style={[
+                  styles.decision,
+                  { fontFamily: theme.fonts.text400, fontSize: 18 },
+                ]}
+              >
+                Não
+              </Text>
+            </TouchableOpacity>
+
+            <TouchableOpacity
+              activeOpacity={0.7}
+              onPress={() => signOut()}
+              style={[
+                styles.decisionButton,
+                { borderColor: theme.colors.primary },
+              ]}
+            >
+              <Text
+                style={[
+                  styles.decision,
+                  { fontFamily: theme.fonts.text400, fontSize: 18 },
+                ]}
+              >
+                Sim
+              </Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+      </Modal>
     </View>
   )
 }
--- src/components/Profile/styles.ts
+++ src/components/Profile/styles.ts
@@ -28,4 +28,41 @@ export const styles = StyleSheet.create({
     fontFamily: theme.fonts.text400,
     color: theme.colors.highlight,
   },
+
+  modalContainer: {
+    position: 'absolute',
+    bottom: 0,
+    width: '100%',
+    height: '20%',
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: theme.colors.secondary80,
+  },
+
+  decision: {
+    fontSize: 20,
+    fontFamily: theme.fonts.title700,
+    color: theme.colors.heading,
+  },
+
+  decisionContainer: {
+    position: 'absolute',
+    bottom: 30,
+    left: 15,
+    justifyContent: 'center',
+    alignItems: 'center',
+    flexDirection: 'row',
+  },
+
+  decisionButton: {
+    height: 60,
+    width: 175,
+    backgroundColor: theme.colors.primary,
+    marginRight: 10,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 8,
+    borderWidth: 1,
+    borderColor: theme.colors.highlight,
+  },
 })
